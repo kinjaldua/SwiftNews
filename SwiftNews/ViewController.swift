3ee9bd7dc06038935a5f58ad9fbc9d6a745b7ce0
Kinjal Dua <kinjal.dua@compassdigital.io>
2020-07-25 12:13:17 -0400

    Initial Commit

---
+++ SwiftNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftNews
+//
+//  Created by Dua, Kinjal on 2020-07-25.
+//  Copyright © 2020 Dua, Kinjal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SwiftNewsTests/SwiftNewsTests.swift
@@ -0,0 +1,34 @@
+//
+//  SwiftNewsTests.swift
+//  SwiftNewsTests
+//
+//  Created by Dua, Kinjal on 2020-07-25.
+//  Copyright © 2020 Dua, Kinjal. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftNews
+
+class SwiftNewsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
