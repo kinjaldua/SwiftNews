1b09a30cb338f4bf2148a9712d9f2e8f86801ae0
Kinjal Dua <kinjal.dua@compassdigital.io>
2020-07-26 10:07:32 -0400

    Code to show navigation bar on NewsListingViewController

--- SwiftNews/App Delegate/SceneDelegate.swift
+++ SwiftNews/App Delegate/SceneDelegate.swift
@@ -18,6 +18,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+        let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        window?.rootViewController = storyBoard.instantiateInitialViewController()
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
