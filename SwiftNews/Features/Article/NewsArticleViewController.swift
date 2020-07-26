95ef01856887a375b0b73b97b133d2e5928e3928
Kinjal Dua <kinjal.dua@compassdigital.io>
2020-07-26 12:38:01 -0400

    Updated NewsArticleViewController to show article data

--- SwiftNews/Features/Article/NewsArticleViewController.swift
+++ SwiftNews/Features/Article/NewsArticleViewController.swift
@@ -12,11 +12,48 @@ class NewsArticleViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     var article: Article?
-    
+
+    lazy var presenter = NewsArticlePresenter(delegate: self)
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
+        tableView.separatorStyle = .none
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        presenter.updateDataSource()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        navigationController?.navigationBar.topItem?.title = ""
+    }
+    
+}
+
+extension NewsArticleViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return presenter.numberOfRows
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let row = presenter.row(for: indexPath)
+        switch row {
+        case .title:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "ArticleTitleCell", for: indexPath) as! ArticleTitleCell
+            cell.configure(presenter.article?.title ?? "")
+            return cell
+        case .body:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "ArticleBodyCell", for: indexPath) as! ArticleBodyCell
+            cell.configure(with: presenter.article)
+            return cell
+
+        case .image:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "ArticleImageCell", for: indexPath) as! ArticleImageCell
+            cell.configure(presenter.article)
+            return cell
+
+        }
     }
     
 }
@@ -24,26 +61,13 @@ class NewsArticleViewController: UIViewController {
 extension NewsArticleViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-       //  let article = presenter.articlesArray?[indexPath.row]
         
     }
 }
 
-extension NewsArticleViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3//presenter.numberOfRows
+extension NewsArticleViewController : NewsArticleDelegate {
+    func reloadTableView() {
+        tableView.reloadData()
     }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        guard let article = presenter.articlesArray?[indexPath.row], let cell = tableView.dequeueReusableCell(withIdentifier: "NewsListCell", for: indexPath) as? NewsListCell  else {
-//            return UITableViewCell()
-//
-//        }
-//        cell.configure(with: article)
-//        return cell
-        return UITableViewCell()
-    }
-    
 }
-
 
