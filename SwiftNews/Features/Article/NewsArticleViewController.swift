baabb66a584223f7b99ea9cdf620d26fc2aba79d
Kinjal Dua <kinjal.dua@compassdigital.io>
2020-07-26 10:35:35 -0400

    Added NewsArticleViewController to show full article

--- SwiftNews/Features/Article/NewsArticleViewController.swift
+++ SwiftNews/Features/Article/NewsArticleViewController.swift
@@ -9,22 +9,41 @@
 import UIKit
 
 class NewsArticleViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    var article: Article?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
+extension NewsArticleViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+       //  let article = presenter.articlesArray?[indexPath.row]
+        
+    }
+}
+
+extension NewsArticleViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3//presenter.numberOfRows
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        guard let article = presenter.articlesArray?[indexPath.row], let cell = tableView.dequeueReusableCell(withIdentifier: "NewsListCell", for: indexPath) as? NewsListCell  else {
+//            return UITableViewCell()
+//
+//        }
+//        cell.configure(with: article)
+//        return cell
+        return UITableViewCell()
+    }
+    
+}
+
+
