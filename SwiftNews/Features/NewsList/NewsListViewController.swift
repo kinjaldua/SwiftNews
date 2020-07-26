9a247e30d01c56a5c84fcb2bcbc8168508115ff8
Kinjal Dua <kinjal.dua@compassdigital.io>
2020-07-26 10:37:22 -0400

    Added code to perform segue to NewsArticleViewController when an article is selected from the list

--- SwiftNews/Features/NewsList/NewsListViewController.swift
+++ SwiftNews/Features/NewsList/NewsListViewController.swift
@@ -12,6 +12,7 @@ class NewsListViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     lazy var presenter: NewsListPresenter = NewsListPresenter(delegate: self)
+    let showArticleSegue = "showArticleSegue"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,15 +23,17 @@ class NewsListViewController: UIViewController {
         super.viewWillAppear(animated)
         presenter.getNews()
     }
-
 }
-
 
 extension NewsListViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-       // let article = presenter.articlesArray?[indexPath.row]
+        let article = presenter.articlesArray?[indexPath.row]
+        //pass article object in segue
+        performSegue(withIdentifier: showArticleSegue, sender: article)
+
     }
+    
 }
 
 extension NewsListViewController: UITableViewDataSource {
@@ -43,17 +46,23 @@ extension NewsListViewController: UITableViewDataSource {
             return UITableViewCell()
 
         }
-        
         cell.configure(with: article)
         return cell
     }
     
 }
 
+extension NewsListViewController {
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == showArticleSegue, let nextViewController = segue.destination as? NewsArticleViewController, let article = sender as? Article {
+            nextViewController.article = article
+        }
+    }
+}
+
 extension NewsListViewController: NewsListDelegate {
     func reloadTableView() {
         tableView.reloadData()
     }
-    
     
 }
